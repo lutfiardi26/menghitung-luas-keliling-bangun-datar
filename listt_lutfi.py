c5bf46c8db0677dfae590cbd931990d677bab91d
lutfiardi26 <117370339+lutfiardi26@users.noreply.github.com>
2022-11-13 20:37:51 +0700

    Delete listt_lutfi.py

--- listt_lutfi.py
+++
@@ -1,9 +0,0 @@
-list1 = ['Lutfi','Ardiansyah']
-list2 = ['Serang','Cikande','Kemayoran']
-print(list1[0])
-print(list2[0])
-
-list1.append('Lutfi_Ardiansyah')
-list2.remove('Serang')
-print (list1[2])
-print (list2[0])
